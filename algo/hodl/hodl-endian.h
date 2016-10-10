e4788e59ea2a6ae31bc27d51b7bda1c9ce5ab657
Alexander Baldeck <alexander.baldeck@spiralwks.com>
2016-10-10 13:36:48 +0800

    only include stdint.h on osx

--- algo/hodl/hodl-endian.h
+++ algo/hodl/hodl-endian.h
@@ -1,7 +1,9 @@
 #ifndef HODL_BYTESWAP_H
 #define HODL_BYTESWAP_H 1
 
+#ifdef __APPLE__
 #include <stdint.h>
+#endif
 
 #define __bswap_constant_16(x) \
      ((unsigned short int) ((((x) >> 8) & 0xff) | (((x) & 0xff) << 8)))
